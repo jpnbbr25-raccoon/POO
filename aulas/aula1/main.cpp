ac7a793d5e39f23dfeb66492ca493bce2a857025
jpnbbr25-raccoon <jpnbbr25@gmail.com>
2026-08-14 12:01:21 -0300

    primeira aula

--- aulas/aula1/main.cpp
+++ aulas/aula1/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
+
+std::string nome = "joao";
+int Valor = 10;
 
 int main(){
 
-std::cout<<"teste simples de commit"<< std::endl;
+std::cout<< nome << std::endl;
+// foi dado convenções de nomenclatura conferir slide da aula do dia 14/08
+// existem varias variaveis que alocam memoria de forma mais eficiente, ocupando apenas o espaco necessario
+//
+
 
 
 return 0;
