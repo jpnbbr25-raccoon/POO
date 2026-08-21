1d5937a63ec0661879206cdb3bbcb2c977f1e129
jpnbbr25-raccoon <jpnbbr25@gmail.com>
2026-08-21 12:11:26 -0300

    fim da aula 2

---
+++ aulas/aula2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath> // Para std::pow
+
+
+int main() {
+
+    int n9 = 10;
+    int n10 = 3;
+
+    double resultado = static_cast<double>(n9) / n10;
+
+    std::cout << resultado << std::endl;
+
+}
